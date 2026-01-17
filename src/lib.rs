da3c9d2645e5aa1cd96c544b53bc83a0deed2751
Ryo Yamashita <qryxip@gmail.com>
2026-01-18 00:20:02 +0900

    feat!: restore v2.0.0-rc.10 except for crate names and ORT version

--- src/lib.rs
+++ src/lib.rs
@@ -68,26 +68,6 @@ pub use self::{
 	error::{Error, ErrorCode, Result}
 };
 
-/// このクレートのフィーチャが指定された状態になっていなければコンパイルエラー。
-#[cfg(feature = "load-dynamic")]
-#[macro_export]
-macro_rules! assert_feature {
-	(cfg(feature = "load-dynamic"), $msg:literal $(,)?) => {};
-	(cfg(not(feature = "load-dynamic")), $msg:literal $(,)?) => {
-		::std::compile_error!($msg);
-	};
-}
-
-/// このクレートのフィーチャが指定された状態になっていなければコンパイルエラー。
-#[cfg(not(feature = "load-dynamic"))]
-#[macro_export]
-macro_rules! assert_feature {
-	(cfg(feature = "load-dynamic"), $msg:literal $(,)?) => {
-		::std::compile_error!($msg);
-	};
-	(cfg(not(feature = "load-dynamic")), $msg:literal $(,)?) => {};
-}
-
 /// The minor version of ONNX Runtime used by this version of `ort`.
 pub const MINOR_VERSION: u32 = ort_sys::ORT_API_VERSION;
 
@@ -98,9 +78,6 @@ pub(crate) static G_ORT_LIB: OnceLock<Arc<libloading::Library>> = OnceLock::new(
 
 #[cfg(feature = "load-dynamic")]
 pub(crate) fn dylib_path() -> &'static String {
-	if cfg!(feature = "__init-for-voicevox") {
-		panic!("`__init-for-voicevox`により禁止されています");
-	}
 	G_ORT_DYLIB_PATH.get_or_init(|| {
 		let path = match std::env::var("ORT_DYLIB_PATH") {
 			Ok(s) if !s.is_empty() => s,
@@ -117,13 +94,6 @@ pub(crate) fn dylib_path() -> &'static String {
 
 #[cfg(feature = "load-dynamic")]
 pub(crate) fn lib_handle() -> &'static libloading::Library {
-	#[cfg(feature = "__init-for-voicevox")]
-	if true {
-		return &G_ENV_FOR_VOICEVOX
-			.get()
-			.expect("`try_init_from`または`try_init`で初期化されていなくてはなりません")
-			.dylib;
-	}
 	G_ORT_LIB.get_or_init(|| {
 		// resolve path relative to executable
 		let path: std::path::PathBuf = dylib_path().into();
@@ -141,151 +111,6 @@ pub(crate) fn lib_handle() -> &'static libloading::Library {
 			.unwrap_or_else(|e| panic!("An error occurred while attempting to load the ONNX Runtime binary at `{}`: {e}", absolute_path.display()));
 		Arc::new(lib)
 	})
-}
-
-#[cfg(feature = "__init-for-voicevox")]
-static G_ENV_FOR_VOICEVOX: once_cell::sync::OnceCell<EnvHandle> = once_cell::sync::OnceCell::new();
-
-#[cfg(feature = "__init-for-voicevox")]
-thread_local! {
-	static G_ORT_API_FOR_ENV_BUILD: std::cell::Cell<Option<NonNull<ort_sys::OrtApi>>> = const { std::cell::Cell::new(None) };
-}
-
-#[cfg(feature = "__init-for-voicevox")]
-#[cfg_attr(docsrs, doc(cfg(feature = "__init-for-voicevox")))]
-#[derive(Debug)]
-pub struct EnvHandle {
-	_env: std::sync::Arc<environment::Environment>,
-	api: AssertSendSync<NonNull<ort_sys::OrtApi>>,
-	#[cfg(feature = "load-dynamic")]
-	dylib: libloading::Library
-}
-
-#[cfg(feature = "__init-for-voicevox")]
-impl EnvHandle {
-	/// インスタンスが既に作られているならそれを得る。
-	///
-	/// 作られていなければ`None`。
-	pub fn get() -> Option<&'static Self> {
-		G_ENV_FOR_VOICEVOX.get()
-	}
-}
-
-#[cfg(feature = "__init-for-voicevox")]
-#[derive(Clone, Copy, Debug)]
-struct AssertSendSync<T>(T);
-
-// SAFETY: `OrtApi`はスレッドセーフとされているはず
-#[cfg(feature = "__init-for-voicevox")]
-unsafe impl Send for AssertSendSync<NonNull<ort_sys::OrtApi>> {}
-
-// SAFETY: `OrtApi`はスレッドセーフとされているはず
-#[cfg(feature = "__init-for-voicevox")]
-unsafe impl Sync for AssertSendSync<NonNull<ort_sys::OrtApi>> {}
-
-/// VOICEVOX CORE用に、`OrtEnv`の作成までをやる。
-///
-/// 一度成功したら以後は同じ参照を返す。
-#[cfg(all(feature = "__init-for-voicevox", feature = "load-dynamic"))]
-#[cfg_attr(docsrs, doc(cfg(all(feature = "__init-for-voicevox", feature = "load-dynamic"))))]
-pub fn try_init_from(filename: &std::ffi::OsStr, tp_options: Option<environment::GlobalThreadPoolOptions>) -> anyhow::Result<&'static EnvHandle> {
-	use anyhow::bail;
-	use ort_sys::ORT_API_VERSION;
-
-	G_ENV_FOR_VOICEVOX.get_or_try_init(|| {
-		let (dylib, api) = unsafe {
-			let dylib = libloading::Library::new(filename)?;
-
-			// この下にある`api()`のものをできるだけ真似る
-
-			let base_getter: libloading::Symbol<unsafe extern "C" fn() -> *const ort_sys::OrtApiBase> = dylib
-				.get(b"OrtGetApiBase")
-				.expect("`OrtGetApiBase` must be present in ONNX Runtime dylib");
-			let base: *const ort_sys::OrtApiBase = base_getter();
-			assert_ne!(base, std::ptr::null());
-
-			let version_string = ((*base).GetVersionString)();
-			let version_string = CStr::from_ptr(version_string).to_string_lossy();
-			tracing::info!("Loaded ONNX Runtime dylib with version '{version_string}'");
-
-			let lib_minor_version = version_string.split('.').nth(1).map_or(0, |x| x.parse::<u32>().unwrap_or(0));
-			match lib_minor_version.cmp(&MINOR_VERSION) {
-				std::cmp::Ordering::Less if cfg!(windows) => {
-					bail!(r"`{dylib:?}`はバージョン{version_string}のONNX Runtimeです。ONNX Runtimeはバージョン1.{MINOR_VERSION}でなくてはなりません");
-				}
-				std::cmp::Ordering::Less => bail!(
-					"`{filename}`で指定されたONNX Runtimeはバージョン{version_string}です。ONNX Runtimeはバージョン1.{MINOR_VERSION}でなくてはなりません",
-					filename = filename.to_string_lossy(),
-				),
-				std::cmp::Ordering::Greater => tracing::warn!(
-					"`{filename}`で指定されたONNX Runtimeはバージョン{version_string}です。対応しているONNX Runtimeのバージョンは1.{MINOR_VERSION}なので、\
-					 互換性の問題があるかもしれません",
-					filename = filename.to_string_lossy(),
-				),
-				std::cmp::Ordering::Equal => {}
-			};
-
-			let api: *const ort_sys::OrtApi = ((*base).GetApi)(ort_sys::ORT_API_VERSION);
-			(dylib, api)
-		};
-		let api = AssertSendSync(NonNull::new(api.cast_mut()).unwrap_or_else(|| panic!("`GetApi({ORT_API_VERSION})`が失敗しました")));
-
-		let _env = create_env(api.0, tp_options)?;
-
-		Ok(EnvHandle { _env, api, dylib })
-	})
-}
-
-/// VOICEVOX CORE用に、`OrtEnv`の作成までをやる。
-///
-/// 一度成功したら以後は同じ参照を返す。
-#[cfg(all(feature = "__init-for-voicevox", any(doc, not(feature = "load-dynamic"))))]
-#[cfg_attr(docsrs, doc(cfg(all(feature = "__init-for-voicevox", not(feature = "load-dynamic")))))]
-pub fn try_init(tp_options: Option<environment::GlobalThreadPoolOptions>) -> anyhow::Result<&'static EnvHandle> {
-	use ort_sys::ORT_API_VERSION;
-
-	G_ENV_FOR_VOICEVOX.get_or_try_init(|| {
-		let api = unsafe {
-			// この下にある`api()`のものをできるだけ真似る
-			let base: *const ort_sys::OrtApiBase = ort_sys::OrtGetApiBase();
-			assert_ne!(base, std::ptr::null());
-			((*base).GetApi)(ort_sys::ORT_API_VERSION)
-		};
-		let api = NonNull::new(api.cast_mut())
-			.unwrap_or_else(|| panic!("`GetApi({ORT_API_VERSION})`が失敗しました。おそらく1.{MINOR_VERSION}より古いものがリンクされています"));
-		let api = AssertSendSync(api);
-
-		let _env = create_env(api.0, tp_options)?;
-
-		Ok(EnvHandle { _env, api })
-	})
-}
-
-#[cfg(feature = "__init-for-voicevox")]
-fn create_env(
-	api: NonNull<ort_sys::OrtApi>,
-	tp_options: Option<environment::GlobalThreadPoolOptions>
-) -> anyhow::Result<std::sync::Arc<environment::Environment>> {
-	use crate::environment::EnvironmentBuilder;
-
-	G_ORT_API_FOR_ENV_BUILD.set(Some(api));
-	let _unset_api = UnsetOrtApi;
-
-	let mut env = EnvironmentBuilder::new().with_name(env!("CARGO_PKG_NAME"));
-	if let Some(tp_options) = tp_options {
-		env = env.with_global_thread_pool(tp_options);
-	}
-	let env = env.commit_internal()?;
-
-	return Ok(env.into());
-
-	struct UnsetOrtApi;
-
-	impl Drop for UnsetOrtApi {
-		fn drop(&mut self) {
-			G_ORT_API_FOR_ENV_BUILD.set(None);
-		}
-	}
 }
 
 /// Returns information about the build of ONNX Runtime used, including version, Git commit, and compile flags.
@@ -313,20 +138,14 @@ static G_ORT_API: OnceLock<ApiPointer> = OnceLock::new();
 /// - Getting the `OrtApi` struct fails, due to `ort` loading an unsupported version of ONNX Runtime.
 /// - Loading the ONNX Runtime dynamic library fails if the `load-dynamic` feature is enabled.
 pub fn api() -> &'static ort_sys::OrtApi {
-	#[cfg(feature = "__init-for-voicevox")]
-	let ptr = G_ENV_FOR_VOICEVOX
-		.get()
-		.map(|&EnvHandle { api: AssertSendSync(api), .. }| api)
-		.or_else(|| G_ORT_API_FOR_ENV_BUILD.get())
-		.expect("`try_init_from`または`try_init`で初期化されていなくてはなりません");
-	#[cfg(all(not(feature = "__init-for-voicevox"), feature = "alternative-backend"))]
+	#[cfg(feature = "alternative-backend")]
 	let ptr = G_ORT_API
 		.get()
 		.expect(
 			"attempted to use `ort` APIs before initializing a backend\nwhen the `alternative-backend` feature is enabled, `ort::set_api` must be called to configure the `OrtApi` used by the library"
 		)
 		.0;
-	#[cfg(all(not(feature = "__init-for-voicevox"), not(feature = "alternative-backend")))]
+	#[cfg(not(feature = "alternative-backend"))]
 	let ptr = G_ORT_API
 		.get_or_init(|| {
 			#[cfg(feature = "load-dynamic")]
